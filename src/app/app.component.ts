c10699291bfdeb669291b539be5431dc41c43f50
Pedro Relvas <pedromiguelrelvas@gmail.com>
2023-04-29 16:56:47 +0100

    added angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pmr-base54-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
