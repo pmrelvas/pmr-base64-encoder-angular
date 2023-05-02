929de5cdd8976b83a468f4e16f85499f4d269072
Pedro Relvas <pedromiguelrelvas@gmail.com>
2023-05-02 23:40:53 +0100

    PMR-3 :: Implemented decoding screen

--- src/app/decode/decode.component.ts
+++ src/app/decode/decode.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EncodingService } from '../shared/services/encoding.service';
+import { FormControl } from '@angular/forms';
+import { Charsets } from '../shared/enums/charsets.enum';
 
 @Component({
   selector: 'pmr-decode',
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class DecodeComponent {
 
+  charsets: BufferEncoding[] = Object.values(Charsets);
+  charsetFormControl = new FormControl(Charsets.UTF8);
+  textToDecodeFormControl = new FormControl('');
+  decodedText = '';
+
+  onDecodeClick(): void {
+    this.decodedText = EncodingService.decodeBase64(this.textToDecodeFormControl.value || '', this.charsetFormControl.value as BufferEncoding);
+  }
 }
--- src/app/shared/services/encoding.service.ts
+++ src/app/shared/services/encoding.service.ts
@@ -12,4 +12,8 @@ export class EncodingService {
     return Buffer.from(strToEncode, charset).toString(EncodingConsts.base64);
   }
 
+  public static decodeBase64(strToDecode: string, charset: BufferEncoding = 'utf-8'): string {
+    return Buffer.from(strToDecode, EncodingConsts.base64).toString(charset);
+  }
+
 }
