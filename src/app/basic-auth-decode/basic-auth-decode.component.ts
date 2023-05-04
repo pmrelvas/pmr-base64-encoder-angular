324032a37518dc739e30b852b758579676066f56
Pedro Relvas <pedromiguelrelvas@gmail.com>
2023-05-04 21:09:52 +0100

    PMR-5 :: implemented basic auth decoding screen

--- src/app/basic-auth-decode/basic-auth-decode.component.ts
+++ src/app/basic-auth-decode/basic-auth-decode.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Charsets } from '../shared/enums/charsets.enum';
+import { EncodingService } from '../shared/services/encoding.service';
 
 @Component({
   selector: 'pmr-basic-auth-decode',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class BasicAuthDecodeComponent {
 
+  charsets: BufferEncoding[] = Object.values(Charsets);
+  charsetFormControl = new FormControl(Charsets.UTF8);
+  textToDecodeFormControl = new FormControl('');
+  decodedUsername: string = '';
+  decodedPassword: string = '';
+
+  onDecodeClick(): void {
+    const decodedText = EncodingService.decodeBase64(this.textToDecodeFormControl.value || '', this.charsetFormControl.value as BufferEncoding);
+    const decodedArr = decodedText.split(':');
+    if (decodedArr.length != 2) {
+      // TODO: add notification toast error
+      return;
+    }
+    this.decodedUsername = decodedArr[0];
+    this.decodedPassword = decodedArr[1];
+  }
 }
